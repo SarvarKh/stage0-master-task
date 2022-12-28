565a7d0ded7e90316e14db16e35c1229dc11532d
Sarvar Khalimov <khalimovsarvar@gmail.com>
2022-12-28 19:36:11 +0500

    Solve Conditions

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,16 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+
+//        second = first + second;
+//        first = second - first;
+//        second = second - first;
+
+        System.out.println(first);
+        System.out.println(second);
     }
 
 }
