d31921259313918aaa1105f2e8f9f391ba9eafaf
Sarvar Khalimov <khalimovsarvar@gmail.com>
2022-12-28 19:22:35 +0500

    Solve LangSyntax

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,18 @@
 package com.epam.langSyntax;
 
+import java.util.Scanner;
+
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
-    }
+        int digit, sum = 0;
 
+        while(number > 0) {
+            digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        System.out.println(sum);
+    }
 }
